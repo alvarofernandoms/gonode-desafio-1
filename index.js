50f83fd00469a1e8332ec2fbabc09fe6b72c43c6
Álvaro Fernando <alvarofernandoms@gmail.com>
2018-11-26 20:12:22 -0200

    Add age form, major and minor pages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -9,10 +10,24 @@ nunjucks.configure('views', {
   watch: true
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'njk')
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   return res.render('initial')
+})
+
+app.get('/major', (req, res) => {
+  return res.render('major', { age: req.query.idade })
+})
+
+app.get('/minor', (req, res) => {
+  return res.render('minor', { age: req.query.idade })
+})
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+  return age >= 18 ? res.redirect(`/major?idade=${age}`) : res.redirect(`/minor?idade=${age}`)
 })
 
 app.listen(3000)
