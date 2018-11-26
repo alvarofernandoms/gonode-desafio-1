c2ae88e55c47959ac8e16f2f0ebe075c34517e82
Álvaro Fernando <alvarofernandoms@gmail.com>
2018-11-26 18:51:18 -0200

    Nunjucks initial setup

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.send('Hello World!')
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.set('view engine', 'njk')
+
+app.get('/', (req, res, next) => {
+  return res.render('initial')
 })
 
 app.listen(3000)
